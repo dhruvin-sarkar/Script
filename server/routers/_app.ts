d811b70ae432eee5764831ff795ba6f3cf2af4cd
dhruvin-sarkar <dhruvinsarkar@outlook.com>
2026-03-18 23:54:36 +0400

    feat(trpc): register new routers in root

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,8 +1,14 @@
 import { router } from '../trpc';
 import { healthRouter } from './health';
+import { userRouter } from './user';
+import { postRouter } from './post';
+import { wakatimeRouter } from './wakatime';
 
 export const appRouter = router({
   health: healthRouter,
+  user: userRouter,
+  post: postRouter,
+  wakatime: wakatimeRouter,
 });
 
 export type AppRouter = typeof appRouter;
