bb298bb5e2fe0db91b2c9b23d96cc54d0ad543a9
dhruvin-sarkar <dhruvinsarkar@outlook.com>
2026-03-19 19:55:56 +0400

    feat(infra): update server/routers/_app.ts

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -6,6 +6,10 @@ import { wakatimeRouter } from './wakatime';
 import { devlogRouter } from './devlog';
 import { uploadRouter } from './upload';
 import { githubRouter } from './github';
+import { blogRouter } from './blog';
+import { forumRouter } from './forum';
+import { notificationRouter } from './notification';
+import { searchRouter } from './search';
 
 export const appRouter = router({
   health: healthRouter,
@@ -15,6 +19,10 @@ export const appRouter = router({
   devlog: devlogRouter,
   upload: uploadRouter,
   github: githubRouter,
+  blog: blogRouter,
+  forum: forumRouter,
+  notification: notificationRouter,
+  search: searchRouter,
 });
 
 export type AppRouter = typeof appRouter;
