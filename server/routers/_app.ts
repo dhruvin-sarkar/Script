e7522c3c2dde8e549ecf28b9149f496b4e06fb99
dhruvin-sarkar <dhruvinsarkar@outlook.com>
2026-03-19 11:49:40 +0400

    chore(router): aggregate routers in root router

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -3,12 +3,18 @@ import { healthRouter } from './health';
 import { userRouter } from './user';
 import { postRouter } from './post';
 import { wakatimeRouter } from './wakatime';
+import { devlogRouter } from './devlog';
+import { uploadRouter } from './upload';
+import { githubRouter } from './github';
 
 export const appRouter = router({
   health: healthRouter,
   user: userRouter,
   post: postRouter,
   wakatime: wakatimeRouter,
+  devlog: devlogRouter,
+  upload: uploadRouter,
+  github: githubRouter,
 });
 
 export type AppRouter = typeof appRouter;
